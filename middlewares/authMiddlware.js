4a913ae4950a8f6ed4daa0318d8224bfef819df9
Naimat Ullah <naimat.official.10@gmail.com>
2024-09-07 12:05:26 +0500

    add customer and update kiran branch

--- middlewares/authMiddlware.js
+++ middlewares/authMiddlware.js
@@ -1,69 +1,66 @@
-import jwt from "jsonwebtoken";
-import { promisify } from "util";
-import AppError from "./../utils/appError.js";
-import catchAsync from "./../utils/catchAsync.js";
+import jwt from 'jsonwebtoken'
+
+import { promisify } from 'util'
+import AppError from './../utils/appError.js'
+import catchAsync from './../utils/catchAsync.js'
 
 export const protect = catchAsync(async (req, res, next) => {
-	// 1) Getting token and check of it's there
-	let token;
+   // Getting token and check of it's there
+   let token
 
-	if (
-		req.headers.authorization &&
-		req.headers.authorization.startsWith("Bearer")
-	) {
-		token = req.headers.authorization.split(" ")[1];
-	}
+   if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+   ) {
+      token = req.headers.authorization.split(' ')[1]
+   }
 
-	if (!token) {
-		return next(
-			new AppError("You are not logged in! Please log in to get access.", 401)
-		);
-	}
+   if (!token) {
+      return next(
+         new AppError(
+            'You are not logged in! Please log in to get access.',
+            401
+         )
+      )
+   }
 
-	// 2) Verification token
-	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+   // Verification token
+   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
 
-	const { userId } = decoded;
+   const { userId } = decoded
 
-	// 3) Determine the model based on the user role in the token
-	const userRole = decoded.role; // Assume role is included in the token payload
+   // Check if user still exists
+   const user = await db('users').where({ userId })
 
-	// 4) Check if user still exists
-	const currentUser = await Model.findById(userId);
+   if (!user) {
+      return next(
+         new AppError(
+            'The token belonging to this user does no longer exist.',
+            401
+         )
+      )
+   }
 
-	if (!currentUser) {
-		return next(
-			new AppError(
-				"The token belonging to this user does no longer exist.",
-				401
-			)
-		);
-	}
+   // GRANT ACCESS TO PROTECTED ROUTE
+   req.user = user
 
-	// 5) Check if user changed password after the token was issued (iat: issued at)
-	if (currentUser.changePasswordAfter(decoded.iat)) {
-		return next(
-			new AppError("User recently changed password! Please log in again.", 401)
-		);
-	}
-
-	// 6) GRANT ACCESS TO PROTECTED ROUTE
-	req.user = currentUser;
-
-	next();
-});
+   next()
+})
 
 // restrictTo is a Wrapper function to return the middleware function
 export const restrictTo = (...roles) => {
-	return (req, res, next) => {
-		// roles is array: ['admin']
+   return (req, res, next) => {
+      // roles is array: ['admin']
 
-		if (!roles.includes(req.user.role)) {
-			return next(
-				new AppError("You do not have permission to perform this action.", 403)
-			); // 403: Forbiden
-		}
+      if (!roles.includes(req.user.role)) {
+         return next(
+            new AppError(
+               'You do not have permission to perform this action.',
+               403
+            )
+         ) // 403: Forbiden
+      }
 
-		next();
-	};
-};
+      next()
+   }
+}
