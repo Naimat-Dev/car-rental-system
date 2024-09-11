231322235fdc8c881c1c98378bb272ecbf98182f
Naimat Ullah <naimat.official.10@gmail.com>
2024-09-11 13:04:18 +0500

    update table names

--- controllers/booking/bookingController.js
+++ controllers/booking/bookingController.js
@@ -22,12 +22,8 @@ export const createCarBooking = catchAsync(async (req, res, next) => {
    }
 
    // Check if the car is available
-<<<<<<< HEAD
    const carStatus = await db('car_status').where({ carId }).first()
-=======
-   const carStatus = await db('carStatus').where({ carId }).first()
 
->>>>>>> test
    if (carStatus.availabilityStatus !== 'available') {
       return next(new AppError('Car is not available for booking.', 400))
    }
@@ -60,29 +56,13 @@ export const createCarBooking = catchAsync(async (req, res, next) => {
       .returning('*')
 
    // Update the car's availability status to 'unavailable'
-<<<<<<< HEAD
-   await db('car_status')
-=======
-   const carStatusUpdate = await db('carStatus')
->>>>>>> test
+   const carStatusUpdate = await db('car_status')
       .where({ carId })
       .update({ availabilityStatus: 'unavailable' })
       .returning('*')
 
-<<<<<<< HEAD
-   const transaction = await db('car_transactions').insert({
-      customerId,
-      bookingId: booking.id,
-      ownerId: car.ownerId,
-      rentalCharges: totalPrice,
-      paymentMethod,
-      paymentDate,
-      status: 'pending',
-   })
-=======
    if (!carStatusUpdate.length) {
       await db('bookings').where({ id: booking.id }).del()
->>>>>>> test
 
       return next(new AppError(`Booking is not create successfully.`, 400))
    }
@@ -97,71 +77,13 @@ export const createCarBooking = catchAsync(async (req, res, next) => {
 })
 
 // // Function to get all booking
-<<<<<<< HEAD
-export const getBooking = getAll('car_bookings')
-=======
+
 export const getBookings = getAll('bookings')
->>>>>>> test
 
 // Function to get a booking by ID
 export const getBookingById = getOne('car_bookings')
 
 // Function to update a booking by ID
-<<<<<<< HEAD
-export const updateCarBooking = catchAsync(async (req, res) => {
-   const { id } = req.params
-   const {
-      rentalStartDate,
-      rentalEndDate,
-      totalDays,
-      initialMileage,
-      totalPrice,
-   } = req.body
-
-   // Check if the booking exists
-   const existingBooking = await db('car_bookings').where({ id }).first()
-
-   if (!existingBooking) {
-      return next(new AppError(`Booking not found by that ID.`, 404))
-   }
-
-   // Optional: If carId or customerId needs to be updated, you can validate them
-   const { carId, customerId } = req.body
-   if (carId) {
-      const car = await db('cars').where({ id: carId }).first()
-      if (!car) {
-         return res.status(404).json({ error: 'Car not found.' })
-      }
-   }
-
-   if (customerId) {
-      const customer = await db('customers').where({ id: customerId }).first()
-      if (!customer) {
-         return res.status(404).json({ error: 'Customer not found.' })
-      }
-   }
-
-   // Update the booking details
-   await db('car_bookings')
-      .where({ id })
-      .update({
-         rentalStartDate: rentalStartDate || existingBooking.rentalStartDate,
-         rentalEndDate: rentalEndDate || existingBooking.rentalEndDate,
-         totalDays: totalDays || existingBooking.totalDays,
-         initialMileage:
-            initialMileage !== undefined
-               ? initialMileage
-               : existingBooking.initialMileage,
-         totalPrice: totalPrice || existingBooking.totalPrice,
-         carId: carId || existingBooking.carId, // Only update if carId is provided
-         customerId: customerId || existingBooking.customerId, // Only update if customerId is provided
-      })
-
-   res.status(200).json({ message: 'Car booking updated successfully.' })
-})
-
-=======
 export const updateCarBooking = updateOne('bookings')
->>>>>>> test
 // Function to delete a  by ID
 export const deleteBookingById = deleteOne('car_bookings')
--- controllers/user/userAddressController.js
+++ controllers/user/userAddressController.js
@@ -1,4 +1,10 @@
-import { createOne, getAll, getOne, updateOne, deleteOne, } from '../handleFactory.js'
+import {
+   createOne,
+   getAll,
+   getOne,
+   updateOne,
+   deleteOne,
+} from '../handleFactory.js'
 import db from '../../config/db.js'
 import catchAsync from '../../utils/catchAsync.js'
 
@@ -19,13 +25,8 @@ export const updateUserAddressById = updateOne('user_address')
 
 //Routes //api/users/addresses/all/
 
-<<<<<<< HEAD
-export const getUserAddressJoin = catchAsync(async (req, res, next) => {
-   const userAddresses = await db('user_address as ua')
-=======
 export const joinUserAddressWithUsers = catchAsync(async (req, res, next) => {
-   const userAddresses = await db('userAddress as ua')
->>>>>>> test
+   const userAddresses = await db('user_address as ua')
       .leftJoin('users as u', 'ua.userId', 'u.id')
       .select('*')
 
@@ -42,67 +43,38 @@ export const joinUserAddressWithUsers = catchAsync(async (req, res, next) => {
 })
 
 // Route /api/users/addresses/all/:id
-export const joinUserAddressWithUsersById = catchAsync(async (req, res, next) => {
-   const { id } = req.params;
+export const joinUserAddressWithUsersById = catchAsync(
+   async (req, res, next) => {
+      const { id } = req.params
 
-<<<<<<< HEAD
-   const userAddress = await db('user_address as ua')
-      .leftJoin('users as u', 'ua.userId', 'u.id') // Join with users table
-=======
-   const userAddress = await db('userAddress as ua')
-      .leftJoin('users as u', 'ua.userId', 'u.id')
->>>>>>> test
-      .select(
-         'ua.id',
-         'ua.address',
-         'ua.city',
-         'ua.zipCode',
-         'ua.state',
-         'u.id as userId',
-         'u.email',
-         'u.name',
-         'u.phoneNumber',
-         'u.status',
-         'u.registrationDate',
-         'u.image',
-         'u.cnic',
-         'u.role',
-         'u.passwordChangedAt'
-      )
-      .where('ua.id', id)
-      .first();
+      const userAddress = await db('user_address as ua')
+         .leftJoin('users as u', 'ua.userId', 'u.id')
+         .select(
+            'ua.id',
+            'ua.address',
+            'ua.city',
+            'ua.zipCode',
+            'ua.state',
+            'u.id as userId',
+            'u.email',
+            'u.name',
+            'u.phoneNumber',
+            'u.status',
+            'u.registrationDate',
+            'u.image',
+            'u.cnic',
+            'u.role'
+         )
+         .where('ua.id', id)
+         .first()
 
-   if (!userAddress) {
-      return next(new AppError('No user address found with that ID', 404));
+      if (!userAddress) {
+         return next(new AppError('No user address found with that ID', 404))
+      }
+
+      res.status(200).json({
+         status: 'success',
+         doc: userAddress,
+      })
    }
-
-   res.status(200).json({
-      status: 'success',
-      doc:userAddress,
-      
-   });
-});
-
-
-<<<<<<< HEAD
-export const getUserAddressWithCards = catchAsync(async (req, res, next) => {
-   const userAddresses = await db('user_address as ua')
-      .leftJoin('users as u', 'ua.userId', 'u.id') // Join with users
-      .leftJoin('cards as c', 'u.id', 'c.userId') // Join with cards
-      .select('*') // Select all fields from the joined tables
-
-   // Remove any sensitive fields (e.g., password, CVV)
-   const addressesWithoutSensitiveData = userAddresses.map(
-      ({ password, passwordResetToken, passwordResetExpires, cvv, ...rest }) =>
-         rest
-   )
-
-   res.status(200).json({
-      status: 'success',
-      doc: {
-         userAddresses: addressesWithoutSensitiveData,
-      },
-   })
-})
-=======
->>>>>>> test
+)
